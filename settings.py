4303b1e6e16047639ee99e123864e7cdf8c102b2
Cara Leong <heycraa@gmail.com>
2017-11-03 01:38:18 +0800

    add small dataset references to settings.py

--- settings.py
+++ settings.py
@@ -6,3 +6,7 @@ CSV_STORE = 'data/store.csv'
 
 CSV_TREATED_TRAIN = 'data/treated/train.csv'
 CSV_TREATED_TEST = 'data/treated/test.csv'
+
+CSV_SMALL_TRAIN = 'data/treated/small_train.csv'
+CSV_SMALL_TEST = 'data/treated/small_test.csv'
+CSV_SMALL_DEV = 'data/treated/small_dev.csv'
