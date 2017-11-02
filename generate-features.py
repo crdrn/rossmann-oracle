e20477d871a56f6974df74a05f659dbd247c3158
Darren Wee <darren.wee@u.nus.edu>
2017-11-02 16:22:17 +0800

    implement generate_is_in_competition feature

--- generate-features.py
+++ generate-features.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 import pandas as pd
-from settings import CSV_TRAIN, CSV_TEST, CSV_STORE
+from settings import CSV_TRAIN, CSV_TEST, CSV_STORE, CSV_TREATED_TRAIN, CSV_TREATED_TEST
+from describe import summarize
 
 # Load the data into a DataFrame
 train = pd.read_csv(CSV_TRAIN, low_memory=False)
@@ -11,4 +12,22 @@ store = pd.read_csv(CSV_STORE, low_memory=False)
 train_store = pd.merge(train, store, on='Store', how='outer')
 test_store = pd.merge(test, store, on='Store', how='outer')
 
+
+def generate_is_in_competition(df):
+    # construct datestamp YYYY-MM-DD for date of competition's opening
+    df['Date'] = pd.to_datetime(df['Date'])
+    df['DateOfCompetitionOpen'] = pd.to_datetime(df.CompetitionOpenSinceYear*10000 + df.CompetitionOpenSinceMonth*100 + 15,format='%Y%m%d')
+
+    # now make the flag
+    df['IsInCompetition'] = (df['DateOfCompetitionOpen'] <= df['Date']) * 1
+    return df
+
+train_store = generate_is_in_competition(train_store)
+test_store = generate_is_in_competition(test_store)
+
+#summarize(train_store)
 #print(train_store)
+
+# save dataframes to csv
+train_store.to_csv(CSV_TREATED_TRAIN, sep=',', index=False)
+test_store.to_csv(CSV_TREATED_TEST, sep=',', index=False)
--- settings.py
+++ settings.py
@@ -3,3 +3,6 @@
 CSV_TRAIN = 'data/train_v2.csv'
 CSV_TEST = 'data/test_v2.csv'
 CSV_STORE = 'data/store.csv'
+
+CSV_TREATED_TRAIN = 'data/treated/train.csv'
+CSV_TREATED_TEST = 'data/treated/test.csv'
