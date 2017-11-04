1713d50e6235aee8c9966c502efd1617641f3791
Darren Wee <darren.wee@u.nus.edu>
2017-11-05 01:32:48 +0800

    add month feature to keras script

--- settings.py
+++ settings.py
@@ -13,7 +13,7 @@ CSV_SMALL_DEV = 'data/treated/small_dev.csv'
 
 CATEGORICAL_FEATURES = ['DayOfWeek', 'Open', 'Promo', 'StateHoliday', 'SchoolHoliday', 'StoreType',
                         'Assortment', 'IsInCompetition', 'Promo2']
-TRAIN_FEATURES = ['DayOfWeek', 'Sales', 'Customers', 'Open', 'Promo', 'StateHoliday',
+TRAIN_FEATURES = ['DayOfWeek', 'Date', 'Sales', 'Customers', 'Open', 'Promo', 'StateHoliday',
                   'SchoolHoliday', 'StoreType','Assortment', 'CompetitionDistance', 'IsInCompetition', 'Promo2']
 TEST_FEATURES = list(TRAIN_FEATURES)
 TEST_FEATURES.remove('Sales')
