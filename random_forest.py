39cb01639274973135739220b03751e89bb271c0
Cara Leong <heycraa@gmail.com>
2017-11-05 18:46:49 +0800

    PEP8 formatting

--- random_forest.py
+++ random_forest.py
@@ -1,13 +1,13 @@
 import numpy as np
 import pandas as pd
 import settings
-import scipy
 from sklearn.ensemble import RandomForestRegressor
+
 
 def format_output(results, datacsv, outfile="output.csv"):
     # Convert all predictions when store is closed to 1
     is_open = pd.read_csv(datacsv)['Open']
-    df = pd.DataFrame(pow(2,(results)), columns=['Sales'])
+    df = pd.DataFrame(pow(2, (results)), columns=['Sales'])
     df.where(is_open == 1, 1, inplace=True)
     df.where(df != 0, 1, inplace=True)
 
@@ -17,6 +17,7 @@ def format_output(results, datacsv, outfile="output.csv"):
     df.to_csv(outfile, index_label='Id')
     return
 
+
 def fix_datatypes(df):
     #  fix missing and non-numeric data
 
@@ -24,27 +25,27 @@ def fix_datatypes(df):
         df = format_stateHoliday(df)
 
     if 'CompetitionDistance' in df:
-        df.loc[:,'CompetitionDistance'] = np.log2(df['CompetitionDistance'].fillna(9999999))
+        df.loc[:, 'CompetitionDistance'] = np.log2(df['CompetitionDistance'].fillna(9999999))
         df.drop(labels=['CompetitionDistance'], axis=1, inplace=True)
 
     if 'Assortment' in df:
         assortment = pd.get_dummies(df['Assortment'])
-        df.loc[:,'AssortmentA'] = assortment['a'] if 'a' in assortment else 0
-        df.loc[:,'AssortmentB'] = assortment['b'] if 'b' in assortment else 0
-        df.loc[:,'AssortmentC'] = assortment['c'] if 'c' in assortment else 0
+        df.loc[:, 'AssortmentA'] = assortment['a'] if 'a' in assortment else 0
+        df.loc[:, 'AssortmentB'] = assortment['b'] if 'b' in assortment else 0
+        df.loc[:, 'AssortmentC'] = assortment['c'] if 'c' in assortment else 0
         df.drop(labels=['Assortment'], axis=1, inplace=True)
 
     if 'StoreType' in df:
         storetype = pd.get_dummies(df['StoreType'])
-        df.loc[:,'storetypeA'] = storetype['a'] if 'a' in storetype else 0
-        df.loc[:,'storetypeB'] = storetype['b'] if 'b' in storetype else 0
-        df.loc[:,'storetypeC'] = storetype['c'] if 'c' in storetype else 0
-        df.loc[:,'storetypeD'] = storetype['d'] if 'd' in storetype else 0
+        df.loc[:, 'storetypeA'] = storetype['a'] if 'a' in storetype else 0
+        df.loc[:, 'storetypeB'] = storetype['b'] if 'b' in storetype else 0
+        df.loc[:, 'storetypeC'] = storetype['c'] if 'c' in storetype else 0
+        df.loc[:, 'storetypeD'] = storetype['d'] if 'd' in storetype else 0
         df.drop(labels=['StoreType'], axis=1, inplace=True)
 
     if 'Date' in df:
         date = pd.to_datetime(df['Date'])
-        df.loc[:,'Day'] = date.map(lambda x: x.month)
+        df.loc[:, 'Day'] = date.map(lambda x: x.month)
         df.drop(labels=['Date'], axis=1, inplace=True)
 
     return df
@@ -63,20 +64,24 @@ def format_stateHoliday(df):
     df.drop(labels=['StateHoliday'], axis=1, inplace=True)
     return df
 
+
 def add_median(df, median):
     return pd.merge(df, median, on='Store', how='outer')
 
+
 def calculate_median(df):
-    median = pd.DataFrame(df.groupby(['Store'], as_index=False).median()[['Store','Sales']])
-    median.columns = ['Store','Median']
+    median = pd.DataFrame(df.groupby(['Store'], as_index=False).median()[['Store', 'Sales']])
+    median.columns = ['Store', 'Median']
     median['Median'] = np.log2(median['Median'])
     return median
+
 
 def add_to_christmas(df):
     today = pd.to_datetime(df['Date'])
     christmas = pd.to_datetime("25/12/2015")
     df['DaysTillChristmas'] = (today - christmas).apply(lambda x: x.days % 182)
     return df
+
 
 def get_features(df, median):
     ''' Gets features to be used in the training/testing '''
@@ -85,6 +90,7 @@ def get_features(df, median):
     df = df[settings.TEST_FEATURES]
     df = fix_datatypes(df)
     return df
+
 
 def main():
     train = pd.read_csv(settings.CSV_SMALL_TRAIN)
@@ -96,16 +102,17 @@ def main():
     X = get_features(train, median)
 
     # log2 the sales score
-    y = np.log(train['Sales']+1)
+    y = np.log(train['Sales'] + 1)
 
     forest = RandomForestRegressor(n_estimators=100, max_depth=10, oob_score=True)
-    forest.fit(X,y)
+    forest.fit(X, y)
 
     # test RandomForest
     test = pd.read_csv(settings.CSV_TREATED_TEST)
-    testX= get_features(test, median)
+    testX = get_features(test, median)
     format_output(forest.predict(testX), settings.CSV_TREATED_TEST)
     return
+
 
 if __name__ == '__main__':
     main()
--- settings.py
+++ settings.py
@@ -13,15 +13,17 @@ CSV_SMALL_DEV = 'data/treated/small_dev.csv'
 
 CATEGORICAL_FEATURES = ['DayOfWeek', 'Open', 'Promo', 'StateHoliday', 'SchoolHoliday', 'StoreType',
                         'Assortment', 'IsInCompetition', 'Promo2']
-TRAIN_FEATURES = ['DayOfWeek', 'Date', 'Store', 'Sales', 'Customers', 'Open', 'Promo', 'StateHoliday',
-                  'SchoolHoliday', 'StoreType','Assortment', 'CompetitionDistance', 'IsInCompetition', 'Promo2',
-                  'Median']
-TRAIN_FEATURES = ['Date','Store','Sales','Customers','Open', 'Promo','Assortment','Promo2']
-TEST_FEATURES = list(TRAIN_FEATURES)
-TEST_FEATURES.remove('Sales')
-ADDED_FEATURES = ['Median','DaysTillChristmas']
-TEST_FEATURES += ADDED_FEATURES
+
 ALL_FEATURES=['Store','DayOfWeek','Date','Sales','Customers','Open','Promo','StateHoliday',
               'SchoolHoliday','StoreType','Assortment','CompetitionDistance',
               'CompetitionOpenSinceMonth','CompetitionOpenSinceYear','Promo2','Promo2SinceWeek',
               'Promo2SinceYear','PromoInterval','DateOfCompetitionOpen','IsInCompetition']
+
+TRAIN_FEATURES = ['DayOfWeek', 'Date', 'Store', 'Sales', 'Customers', 'Open', 'Promo', 'StateHoliday',
+                  'SchoolHoliday', 'StoreType','Assortment', 'CompetitionDistance', 'IsInCompetition', 'Promo2',
+                  'Median']
+# TRAIN_FEATURES = ['Date','Store','Sales','Customers','Open', 'Promo','Assortment','Promo2']
+TEST_FEATURES = list(TRAIN_FEATURES)
+TEST_FEATURES.remove('Sales')
+ADDED_FEATURES = ['Median','DaysTillChristmas']
+TEST_FEATURES += ADDED_FEATURES
