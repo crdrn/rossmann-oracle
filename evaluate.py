fe03802d59a1220d6a6a4a0c0b0e90538dad569d
Cara Leong <heycraa@gmail.com>
2017-11-06 00:17:47 +0800

    Write evaluation.py

--- evaluate.py
+++ evaluate.py
@@ -1,5 +1,5 @@
 import settings
-import pandas
+import pandas as pd
 
 def evaluate_predictions(predictions, actual):
     """ Returns RMSPE given lists of predictions and corresponding actual sales """
@@ -9,4 +9,16 @@ def evaluate_predictions(predictions, actual):
         sum += pow((float(real) - estimate)/real, 2)
     return pow(sum/len(actual), 0.5)
 
-def
+def evaluate(prediction_csv):
+    answers = pd.read_csv(settings.CSV_SMALL_TEST)['Sales']
+    prediction = pd.read_csv(prediction_csv)['Sales']
+    print("RMSPE: {}".format(evaluate_predictions(prediction, answers)))
+
+
+def main():
+    prediction_file = 'output.csv'
+    print("Evaluating RMSPE of '{}' compared to '{}'".format(prediction_file, settings.CSV_SMALL_TEST))
+    evaluate(prediction_file)
+
+if __name__ == '__main__':
+    main()
